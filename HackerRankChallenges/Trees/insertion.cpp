85e397d06a7a353d2889f1a220f31c8d0f591c2f
Kamran Janjua <kamranejaz98@gmail.com>
2017-12-07 07:00:41 +0500

    Create insertion.cpp

---
+++ HackerRankChallenges/Trees/insertion.cpp
@@ -0,0 +1,25 @@
+/*
+Node is defined as 
+
+typedef struct node
+{
+   int data;
+   node * left;
+   node * right;
+}node;
+
+*/
+
+node * insert(node * root, int value) {
+    if(root == NULL){
+        node* newNode = new node();
+	    newNode->data = value;
+	    newNode->left = newNode->right = NULL;
+        root = newNode;
+    }
+    else if (value <= root->data)
+        root->left = insert(root->left, value);
+    else
+       root->right = insert(root->right, value);
+    return root;
+}
