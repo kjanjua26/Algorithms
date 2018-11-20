31080171b3825002ac8bdbb32f0f1a970ca98968
Kamran Janjua <kamranejaz98@gmail.com>
2018-11-20 14:46:28 +0500

    Create bi-pipe.c

---
+++ OS-Labs/Lab_10_Pipes/bi-pipe.c
@@ -0,0 +1,38 @@
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+/*
+	Bidirectional Communication between two processes, parent and child, using pipes in C.
+*/
+int main(){
+	int parent[2]; // reading and writing for parent. 
+	int child[2]; // reading and writing for child. 
+	char buff[50];
+	pipe(parent); pipe(child);
+	int pid = fork(); // forking process. 
+	if(pid == -1){
+		perror("Fork Unsuccessful.");
+		exit(-1);
+	}
+	if(pid == 0){
+		char child_writes[] = "Hi Parent Process!";
+		int in, out;
+		in = child[0];
+		out = parent[1];
+		read(in, buff, 50);
+		printf("Parent Says: %s\n", buff);
+		write(out, child_writes, strlen(child_writes) + 1); // send the message to parent. 
+	}
+	else{
+		char parent_writes[] = "Hello Child Process!";
+		int in, out;
+		in = parent[0];
+		out = child[1];
+	    write(out, parent_writes, strlen(parent_writes) + 1); // send message to the child. 
+	    read(in, buff, 50); // reading the message from the parent.
+	    printf("Child Says: %s\n", buff);
+	}
+	return 0;
+}
